2781df9b6319162661ba4472ab464d84a70a7b53
Mohamed Essam <Mohamed Essam@Mohamed-Essam>
2018-04-13 20:15:14 +0200

    DataBase for login

--- src/Database/dbConnection.java
+++ src/Database/dbConnection.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class dbConnection {
-    private static final String SQCONN="jdbo:sqlite:librarymanagementsystem.sqlite";
+    private static final String SQCONN="jdbc:sqlite:librarymanagementsystem.sqlite";
     
     public static Connection getConnection() throws SQLException{
         try {
