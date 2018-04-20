22b78d2ce65a2fe4d7a70e212aae506d6d7bc94d
Mohamed Essam <Mohamed Essam@Mohamed-Essam>
2018-04-21 01:10:32 +0200

    Login Ready

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -9,7 +9,7 @@ public class Login extends Application{
 
     @Override
     public void start(Stage stage)throws Exception{
-        Parent root = (Parent) FXMLLoader.load(getClass().getResource("New.fxml"));
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("Login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Library Management System");
