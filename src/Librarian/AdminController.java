976e10f1b4b3fbc6ff3bab0eb0dcc586a8cf6516
Mohamed Essam <Mohamed Essam@Mohamed-Essam>
2018-04-21 01:21:24 +0200

    Login Ready

--- src/Librarian/AdminController.java
+++ src/Librarian/AdminController.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Librarian;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,6 +17,39 @@ import javafx.fxml.Initializable;
  */
 public class AdminController implements Initializable {
 
+    @FXML
+    private Tab bookissue;
+    @FXML
+    private JFXTextField enterbookid;
+    @FXML
+    private Text bookname;
+    @FXML
+    private Text author;
+    @FXML
+    private JFXTextField entermemberid;
+    @FXML
+    private Text membername;
+    @FXML
+    private Text contact;
+    @FXML
+    private JFXButton issue;
+    @FXML
+    private Tab renew;
+    @FXML
+    private JFXTextField bookId;
+    @FXML
+    private JFXButton submission;
+    @FXML
+    private JFXButton addmember;
+    @FXML
+    private JFXButton addbook;
+    @FXML
+    private JFXButton viewmembers;
+    @FXML
+    private JFXButton viewbooks;
+    @FXML
+    private JFXButton settings;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +57,37 @@ public class AdminController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleIssueBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleRenewBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSubmissionBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAddMemBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAddBookBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleViewMemBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleViewBooksBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSettingsBtnAction(ActionEvent event) {
+    }
     
 }
